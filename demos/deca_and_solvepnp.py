eec94a901cf2e60e327f6ab8d4483730d7dab8b2
Massimo Bortolamei <massimo.bortolamei@supsi.ch>
2024-01-08 11:11:09 +0100

    clean code, add args to save various data

--- demos/deca_and_solvepnp.py
+++ demos/deca_and_solvepnp.py
@@ -28,10 +28,10 @@ import time
 import open3d as o3d
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-full_res_slow = True
+#full_res_slow = True
 full_res_slow = False
 
-use_original = True
+#use_original = True
 use_original = False
 
 if use_original:
@@ -50,24 +50,34 @@ def main(args):
     savefolder = args.savefolder
     device = args.device
     os.makedirs(savefolder, exist_ok=True)
-
     # load test images 
     testdata = datasets.TestData(args.inputpath, iscrop=args.iscrop, face_detector=args.detector, sample_step=args.sample_step)
 
     # Load camera matrix
-    data = np.load(r'Python Camera Calibration\calibration_data_webcam_side.npz')
+    calibration_data = args.calibrationData
+    data = np.load(calibration_data)
     camera_matrix = data['camera_matrix']
     dist_coeffs = data['dist_coeffs']
+    
     # Ensure that camera_matrix and dist_coeffs are also float32 and have the correct shapes
     camera_matrix = np.asarray(camera_matrix, dtype=np.float32)
     dist_coeffs = np.asarray(dist_coeffs, dtype=np.float32)
 
     print('camera_matrix', camera_matrix, '\n dist_coeffs', dist_coeffs)
+
     # run DECA
+
+    # whether to use FLAME texture model to generate uv texture map
     deca_cfg.model.use_tex = args.useTex
+
+    # pytorch3d or standard(default)
     deca_cfg.rasterizer_type = args.rasterizer_type
+    # whether to extract texture from input image as the uv texture map, set false if you want albedo map from FLAME model
     deca_cfg.model.extract_tex = args.extractTex
+    
     deca = DECA(config = deca_cfg, device=device)
+    
+    # track various execution times
     total_encode_time = 0
     total_decode_time = 0
     total_landmark_time = 0
@@ -76,13 +86,14 @@ def main(args):
     decoding_times = []
     landmark_times = []
     solvepnp_times = []
+    # results
     distances = []
     all_landmarks3D = np.empty((len(testdata), 68, 3))
     all_landmarks2D = np.empty((len(testdata), 68, 2))
     
     # Explicitly specify the flag (try different flags if necessary)
-    #flags = cv2.SOLVEPNP_EPNP  # Example flag, adjust as needed
-    # for i in range(len(testdata)):
+    # flags = cv2.SOLVEPNP_EPNP  # Example flag, adjust as needed
+
     print("FULL RES SLOW ", full_res_slow, "deca original ",use_original)
     for i in tqdm(range(len(testdata))):
         name = testdata[i]['imagename']
@@ -124,7 +135,7 @@ def main(args):
             #landmarks2D = opdict['landmarks2d'][0].cpu().numpy()
             
             eye_distance = math.dist(landmarks3D[39], landmarks3D[42])
-            scaling_factor = 3/eye_distance # in cm
+            scaling_factor = 3/eye_distance # 3 cm between real life corresponing points (eyes inner corners)
             landmarks3D = np.ascontiguousarray(landmarks3D, dtype=np.float32) * scaling_factor
             eye_distance2 = math.dist(landmarks3D[39], landmarks3D[42])
             print(f'\nImage {i}: eye distance {eye_distance} , scaling factor {scaling_factor} - AFTER SCALING eye_distance2 {eye_distance2}')
@@ -154,6 +165,7 @@ def main(args):
             #     print("orig_visdict['landmarks3d'] ",orig_visdict['landmarks3d'][0].cpu().numpy()[:, :3])
             #breakpoint()
             print("BEFORE PNP - landmarks2Dfullres ",landmarks2Dfullres,"landmarks3D",landmarks3D.shape, landmarks3D)
+            
             # ---- SOLVEPNP ----
             start_solvepnp = time.time()
             # Call solvePnP to estimate the pose
@@ -178,18 +190,46 @@ def main(args):
             
             print("AFTER PNP - translation_vector ", translation_vector.shape, translation_vector)
                 
-              
-            draw_points(i, original_image[0], landmarks2Dfullres, testdata[i])
-            save_landmarks3d_ply(i, landmarks3D)
+        # enable or disable to manage performances
+        if args.drawLandmarks2d:
+            draw_points(i, original_image[0], landmarks2Dfullres, testdata[i], args)
             #draw_points(i, original_image[0], landmarks2D, testdata[i])
-        ## ONLY TO PRINT ORIGINAL RESULTS ##
+        if args.saveLandmarks3dPly:
+            save_landmarks3d_ply(i, landmarks3D, args)
+        if args.saveLandmarksTxt:
+            save_landmarks2d3d_txt(i, landmarks2Dfullres, landmarks3D, args)
+        if args.saveDistances:
+            save_distances(i, distance, args)
+    
+# ---- outside loop, after all processing ended
+    for i in range(len(testdata)):
+        print(f"Image ",i," - Encode time -", encoding_times[i])
+        print(f"Image ",i," - Decode time -", decoding_times[i])
+        print(f"Image ",i," - Landmark time -", landmark_times[i])
+        print(f"Image ",i," - Solvepnp time -", solvepnp_times[i])
+        print(f"Image ",i," - Distance is -", distances[i])
+
+    print(f"Total encoding time for all images - {total_encode_time}")
+    print(f"Total decoding time for all images - {total_decode_time}")
+    print(f"Average encoding time per image - {total_encode_time / len(testdata)}")
+    print(f"Average decoding time per image - {total_decode_time / len(testdata)}")   
+    print(f"Total Landmark time for all images - {total_landmark_time}")
+    print(f"Total Solvepnp time for all images - {total_solvepnp_time}")
+    
+    if args.saveAllLandmarksAndDistances:
+        save_allLandmarksAndDistances(all_landmarks3D, all_landmarks3D, distances, args)
+    
+    # print('all_landmarks2D is ',all_landmarks2D)
+    #breakpoint()
+        # ---- ONLY TO PRINT ORIGINAL RESULTS ----
         # opdict, visdict = deca.decode(codedict) #tensor
         # tform = testdata[i]['tform'][None, ...]
         # tform = torch.inverse(tform).transpose(1,2).to(device)
         # original_image = testdata[i]['original_image'][None, ...].to(device)
         # _, orig_visdict = deca.decode(codedict, render_orig=True, original_image=original_image, tform=tform)    
         # orig_visdict['inputs'] = original_image 
- 
+           
+#---- LEGACY ARGUMENTS FROM ORIGINAL DECA LIBRARY - use at own discretion ----
 #         if args.saveDepth or args.saveKpt or args.saveObj or args.saveMat or args.saveImages:
 #             os.makedirs(os.path.join(savefolder, name), exist_ok=True)
 # ##        -- save results
@@ -197,9 +237,9 @@ def main(args):
 #             depth_image = deca.render.render_depth(opdict['trans_verts']).repeat(1,3,1,1)
 #             visdict['depth_images'] = depth_image
 #             cv2.imwrite(os.path.join(savefolder, name, name + '_depth.jpg'), util.tensor2image(depth_image[0]))
-#         if args.saveKpt:
-#             np.savetxt(os.path.join(savefolder, name, name + '_kpt2d.txt'), opdict['landmarks2d'][0].cpu().numpy())
-#             np.savetxt(os.path.join(savefolder, name, name + '_kpt3d.txt'), opdict['landmarks3d'][0].cpu().numpy())
+        # if args.saveKpt:
+        #      np.savetxt(os.path.join(savefolder, name, name + '_kpt2d.txt'), opdict['landmarks2d'][0].cpu().numpy())
+        #      np.savetxt(os.path.join(savefolder, name, name + '_kpt3d.txt'), opdict['landmarks3d'][0].cpu().numpy())
 #         if args.saveObj:
 #             deca.save_obj(os.path.join(savefolder, name, name + '.obj'), opdict)
 #         if args.saveMat:
@@ -222,25 +262,6 @@ def main(args):
 #                     image = util.tensor2image(orig_visdict[vis_name][0])
 #                     cv2.imwrite(os.path.join(savefolder, name, 'orig_' + name + '_' + vis_name +'.jpg'), util.tensor2image(orig_visdict[vis_name][0]))
     # print(f'-- please check the results in {savefolder}')
-    for i in range(len(testdata)):
-        print(f"Image ",i," - Encode time -", encoding_times[i])
-        print(f"Image ",i," - Decode time -", decoding_times[i])
-        print(f"Image ",i," - Landmark time -", landmark_times[i])
-        print(f"Image ",i," - Solvepnp time -", solvepnp_times[i])
-        print(f"Image ",i," - Distance is -", distances[i])
-
-    print(f"Total encoding time for all images - {total_encode_time}")
-    print(f"Total decoding time for all images - {total_decode_time}")
-    print(f"Average encoding time per image - {total_encode_time / len(testdata)}")
-    print(f"Average decoding time per image - {total_decode_time / len(testdata)}")   
-    print(f"Total Landmark time for all images - {total_landmark_time}")
-    print(f"Total Solvepnp time for all images - {total_solvepnp_time}")
-    
-    suffix = 'slow' if full_res_slow else 'fast'
-    np.savez('./demos/all_landmarks3D'+ suffix+'.npz', all_landmarks3D = all_landmarks3D)
-    np.savez('./demos/all_landmarks2D'+ suffix+'.npz', all_landmarks2D = all_landmarks2D)
-    # print('all_landmarks2D is ',all_landmarks2D)
-    #breakpoint()
 
 def convert_normalized_to_pixels(normalized_points, image_width, image_height, imageData):
     pixel_points = []
@@ -267,7 +288,7 @@ def convert_normalized_to_pixels(normalized_points, image_width, image_height, i
     #breakpoint()
     return pixel_points
 
-def draw_points(i, image_tensor, landmarks2D, imageData):
+def draw_points(i, image_tensor, landmarks2D, imageData, args):
     image_numpy = (image_tensor.permute(1, 2, 0).cpu().detach().numpy() * 255).astype(np.uint8)
     for point in landmarks2D:
          x, y = int(round(point[0])), int(round(point[1])) 
@@ -278,28 +299,34 @@ def draw_points(i, image_tensor, landmarks2D, imageData):
     cv2.rectangle(image_numpy, (int(left), int(top)), (int(right), int(bottom)), (0, 255, 0), 2)
     #breakpoint()
 
-    cv2.imwrite(f"./TestSamples/result_{i}_with_landmarks.png", image_numpy)   
+    cv2.imwrite(f"{args.savefolder}/result_{i}_with_landmarks.png", image_numpy)   
     
     #cv2.imshow('Landmarks', image_numpy)
     #cv2.waitKey(0)
     #cv2.destroyAllWindows()
 
-def draw_landmarks(i, landmarks2Doriginal):
-    landmarks2Doriginal = landmarks2Doriginal.transpose(1, 2, 0)
-    landmarks2Doriginal = (landmarks2Doriginal * 255).astype(np.uint8)
-    cv2.imwrite(f"./TestSamples/original_result_{i}_with_landmarks.png", landmarks2Doriginal)     
-
-def save_landmarks3d_ply(i, landmarks3D):
+def save_landmarks3d_ply(i, landmarks3D, args):
     #breakpoint()
 
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(landmarks3D)
-    save_path = f"./TestSamples/{i}_kpt3d.ply"
+    save_path = f"{args.savefolder}/{i}_kpt3d.ply"
     # Write the point cloud to a PLY file with the header
     o3d.io.write_point_cloud(save_path, pcd)
+def save_landmarks2d3d_txt(i, landmarks2Dfullres, landmarks3D, args):
+    np.savetxt(f'{args.savefolder}/{i}_landmarks2d.txt', landmarks2Dfullres)
+    np.savetxt(f'{args.savefolder}/{i}_landmarks3d.txt', landmarks3D)  
+    
+def save_distances(i, distance, args):
+    np.savetxt(f'{args.savefolder}/{i}_distance.txt', distance)  
+    
+def save_allLandmarksAndDistances(all_landmarks3D, all_landmarks2D, distances, args):
+    suffix = 'slow' if full_res_slow else 'fast'
+    np.savez(f'{args.savefolder}/all_landmarks3D'+ suffix+'.npz', all_landmarks3D = all_landmarks3D)
+    np.savez(f'{args.savefolder}/all_landmarks2D'+ suffix+'.npz', all_landmarks2D = all_landmarks2D)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='DECA: Detailed Expression Capture and Animation')
+    parser = argparse.ArgumentParser(description='DECA + SolvePnP')
 
     parser.add_argument('-i', '--inputpath', default='TestSamples/examples', type=str,
                         help='path to the test data, can be image folder, image path, image list, video')
@@ -317,25 +344,40 @@ if __name__ == '__main__':
     # rendering option
     parser.add_argument('--rasterizer_type', default='standard', type=str,
                         help='rasterizer type: pytorch3d or standard' )
-    parser.add_argument('--render_orig', default=True, type=lambda x: x.lower() in ['true', '1'],
-                        help='whether to render results in original image size, currently only works when rasterizer_type=standard')
+    # parser.add_argument('--render_orig', default=True, type=lambda x: x.lower() in ['true', '1'],
+    #                     help='whether to render results in original image size, currently only works when rasterizer_type=standard')
     # save
     parser.add_argument('--useTex', default=False, type=lambda x: x.lower() in ['true', '1'],
                         help='whether to use FLAME texture model to generate uv texture map, \
                             set it to True only if you downloaded texture model' )
     parser.add_argument('--extractTex', default=True, type=lambda x: x.lower() in ['true', '1'],
                         help='whether to extract texture from input image as the uv texture map, set false if you want albeo map from FLAME mode' )
-    parser.add_argument('--saveVis', default=True, type=lambda x: x.lower() in ['true', '1'],
-                        help='whether to save visualization of output' )
+    # parser.add_argument('--saveVis', default=True, type=lambda x: x.lower() in ['true', '1'],
+    #                     help='whether to save visualization of output' )
     parser.add_argument('--saveKpt', default=False, type=lambda x: x.lower() in ['true', '1'],
                         help='whether to save 2D and 3D keypoints' )
-    parser.add_argument('--saveDepth', default=False, type=lambda x: x.lower() in ['true', '1'],
-                        help='whether to save depth image' )
-    parser.add_argument('--saveObj', default=False, type=lambda x: x.lower() in ['true', '1'],
-                        help='whether to save outputs as .obj, detail mesh will end with _detail.obj. \
-                            Note that saving objs could be slow' )
+    # parser.add_argument('--saveDepth', default=False, type=lambda x: x.lower() in ['true', '1'],
+    #                     help='whether to save depth image' )
+    # parser.add_argument('--saveObj', default=False, type=lambda x: x.lower() in ['true', '1'],
+    #                     help='whether to save outputs as .obj, detail mesh will end with _detail.obj. \
+    #                         Note that saving objs could be slow' )
     parser.add_argument('--saveMat', default=False, type=lambda x: x.lower() in ['true', '1'],
                         help='whether to save outputs as .mat' )
     parser.add_argument('--saveImages', default=False, type=lambda x: x.lower() in ['true', '1'],
                         help='whether to save visualization output as seperate images' )
+    
+    # Additional Arguments not in original lib
+    parser.add_argument('--calibrationData', default="Python Camera Calibration/calibration_data_webcam_side.npz", type=str,
+                        help='calibration data with camera matrix and distortion coefficients as result of manual calibration' )
+    parser.add_argument('--drawLandmarks2d', default=False, type=lambda x: x.lower() in ['true', '1'],
+                        help='draw the full res landmarks2d on top of the original image' )
+    parser.add_argument('--saveLandmarks3dPly', default=False, type=lambda x: x.lower() in ['true', '1'],
+                        help='output a point cloud representation of the 3d landmarks' )
+    parser.add_argument('--saveLandmarksTxt', default=False, type=lambda x: x.lower() in ['true', '1'],
+                        help='save landmarks 2d and 3d as txt for each input image' )
+    parser.add_argument('--saveDistances', default=False, type=lambda x: x.lower() in ['true', '1'],
+                        help='save distance between face and camera for each input image' )
+    parser.add_argument('--saveAllLandmarksAndDistances', default=False, type=lambda x: x.lower() in ['true', '1'],
+                        help='save one big file for each of landmarks 2d, 3d, and distances, but with data for all input images' )
+            
     main(parser.parse_args())
