4fde02df6d065e083e9d09bf64c3fdc292bbb606
Massimo Bortolamei <massimo.bortolamei@supsi.ch>
2024-03-20 16:13:41 +0100

    resize refactor with auto height keeping ratio and collect in folder

--- Python Camera Calibration/resize.py
+++ Python Camera Calibration/resize.py
@@ -7,12 +7,11 @@ import argparse
 parser = argparse.ArgumentParser(description='Resize all JPEG images in a specified directory.')
 parser.add_argument('-path', '--path', type=str, required=True, help='Directory containing JPEG files to resize.')
 parser.add_argument('-width', '--width', type=int, required=True, help='New width for the resized images.')
-parser.add_argument('-height', '--height', type=int, required=True, help='New height for the resized images.')
+# parser.add_argument('-height', '--height', type=int, required=True, help='New height for the resized images.')
 args = parser.parse_args()
 
 # Extract the width, height, and path from the arguments
 new_width = args.width
-new_height = args.height
 target_directory = args.path
 
 
@@ -25,11 +24,15 @@ pattern = os.path.join(target_directory, '*.jpg')
 # Use glob to get all the jpg files in that directory
 jpg_files = glob.glob(pattern)
 
-print(f"Resize to {new_width}x{new_height} - fouhd {jpg_files} images")
+# print(f"Resize to {new_width}x{new_height} - found {jpg_files} images")
 # Loop through all the files, read and resize them using OpenCV
 for jpg_file in jpg_files:
     # Read the image
     img = cv2.imread(jpg_file)
+    
+    current_ratio = img.shape[1] / img.shape[0]
+    new_height = int(new_width / current_ratio)
+    print(f"current ratio {current_ratio}. resizing to {new_width} x {new_height}")
     
     # Check if image is read correctly
     if img is not None:
@@ -39,7 +42,7 @@ for jpg_file in jpg_files:
         # Extract the base name without its extension
         base_name = os.path.splitext(os.path.basename(jpg_file))[0]
         resized_file_name = f"{base_name}_{new_width}x{new_height}.jpg"
-        resized_file_path = os.path.join(target_directory, resized_file_name)
+        resized_file_path = os.path.join(target_directory,f"{new_width}x{new_height}", resized_file_name)
         cv2.imwrite(resized_file_path, resized_image)
         print(f"Resizing {jpg_file}")
         
