11e6582c5999fe48c18b06472f8d3e1d78a86613
Massimo Bortolamei <massimo.bortolamei@supsi.ch>
2024-03-20 16:14:41 +0100

    calibration refactor with arguments

--- Python Camera Calibration/calibration.py
+++ Python Camera Calibration/calibration.py
@@ -1,6 +1,21 @@
 import numpy as np
 import cv2 as cv
 import glob
+import argparse
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Set up argument parsing
+parser = argparse.ArgumentParser(description='Calibrate camera based on input images.')
+parser.add_argument('-imagesFolder', '--imagesFolder', type=str, required=True, help='Directory containing JPEG files to use for calibration.')
+parser.add_argument('-outputPath', '--outputPath', type=str, required=True, help='Full output path')
+parser.add_argument('-outputName', '--outputName', type=str, required=True, help='Output filename without extension')
+
+args = parser.parse_args()
+imagesFolder = args.imagesFolder
+outputPath = args.outputPath
+outputName = args.outputName
 
 # Termination criteria for the corner sub-pixel algorithm
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -8,93 +23,33 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # Prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 rows = 6
 columns = 9
-
-imagesFolder = './images_original'
-imageToUndistort = '/left14.jpg'
-calibrationResultImage = 'calibresult_original.png'
-calibrationData = 'calibration_data_original'
-
-
-imagesFolder = './images_webcam'
-imageToUndistort = '/img (14).jpg'
-calibrationResultImage = 'calibresult_webcam.png'
-calibrationData = 'calibration_data_webcam'
-
-imagesFolder = './images_webcam_cut'
-imageToUndistort = '/img (11).jpg'
-calibrationResultImage = 'calibresult_webcam_cut.png'
-calibrationData = 'calibration_data_webcam_cut'
-
-imagesFolder = './images_webcam_closer'
-imageToUndistort = '/img (15).jpg'
-calibrationResultImage = 'calibresult_webcam_closer.png'
-calibrationData = 'calibration_data_webcam_closer'
-
-imagesFolder = './images_iphone_back'
-imageToUndistort = '/img (16).jpg'
-calibrationResultImage = 'calibresult_iphone.png'
-calibrationData = 'calibration_data_iphone_back'
-
-
-imagesFolder = './images_iphone_back_2'
-imageToUndistort = '/img (14).jpg'
-calibrationResultImage = 'calibresult_iphone2.png'
-calibrationData = 'calibration_data_iphone_back_2'
-
-imagesFolder = './images_webcam_side'
-imageToUndistort = '/img (6).jpg'
-calibrationResultImage = 'calibresult_webcam_side.png'
-calibrationData = 'calibration_data_webcam_side'
-
-imagesFolder = './images_webcam_side_640'
-imageToUndistort = '/img (6).jpg'
-calibrationResultImage = 'calibresult_webcam_side_640.png'
-calibrationData = 'calibration_data_webcam_side_640'
-
-imagesFolder = './images_webcam_side_640_2'
-imageToUndistort = '/img (6)_640x360.jpg'
-calibrationResultImage = 'calibresult_webcam_side_640_2.png'
-calibrationData = 'calibration_data_webcam_side_640_2'
-
-imagesFolder = './images_webcam_razer_640'
-imageToUndistort = '/img (6)_640x360.jpg'
-calibrationResultImage = 'calibresult_webcam_side_640_2.png'
-calibrationData = 'calibration_data_razer_640'
-
-imagesFolder = './images_lucidcamera_640'
-imageToUndistort = '/img (15)_640x360.jpg'
-calibrationResultImage = 'calibresult_img (15).png'
-calibrationData = 'calibration_data_lucid_640'
-
-imagesFolder = './images_lucidcamera2_640'
-imageToUndistort = '/img (15)_640x360.jpg'
-calibrationResultImage = 'calibresult_img (15).png'
-calibrationData = 'calibration_data_lucid2_640'
-
-# Use this, since it's last
-imagesFolder = './images_lucidcamera3_640'
-imageToUndistort = '/img (15)_640x360.jpg'
-calibrationResultImage = 'calibresult_img (15).png'
-calibrationData = 'calibration_data_lucid3_640'
-
-objp = np.zeros((rows*columns, 3), np.float32)
-objp[:, :2] = np.mgrid[0:rows, 0:columns].T.reshape(-1, 2)
-
-# Arrays to store object points and image points from all images.
-objpoints = []  # 3d points in real-world space
-imgpoints = []  # 2d points in image plane.
-
-# Change the directory and image format according to your setup.
-images = glob.glob(imagesFolder+'/*.jpg')
-print(f"Images found: {len(images)}")
-
 # Define the size of chessboard squares in millimeters.
 square_size = 23  # Adjust the square size to the actual size of your chessboard squares.
 
+# Use this, since it's last
+# imagesFolder = './images_lucidcamera3_640'
+# imageToUndistort = '/img (15)_640x480.jpg'
+# calibrationResultImage = 'calibresult_img (15).png'
+# calibrationData = 'calibration_data_lucid3_640'
+
+objp = np.zeros((rows*columns, 3), np.float32)
+objp[:, :2] = np.mgrid[0:rows, 0:columns].T.reshape(-1, 2)
 # Prepare object points with actual square size.
 objp = objp * square_size
-#print("objp is ", objp)
 
+# Arrays to store object points and image points from all images.
+# used for error calculation
+objpoints = []  # 3d points in real-world space
+imgpoints = []  # 2d points in image plane.
+
+# Gather all images in folder
+images = glob.glob(imagesFolder+'/*.jpg')
+print(f"Images found: {len(images)}")
+
+
+
+#print("objp is ", objp)
+# breakpoint()
 for fname in images:
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -116,6 +71,7 @@ for fname in images:
         img = cv.drawChessboardCorners(img, (rows, columns), corners2, ret)
         cv.imshow('img', img)
         cv.waitKey(500)
+        # breakpoint()
 
 cv.destroyAllWindows()
 
@@ -123,22 +79,24 @@ cv.destroyAllWindows()
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 print("Distortion coefficients:", dist, "\nCamera matrix:", mtx)
 print("tvecs:", tvecs)
-print("distance:", np.linalg.norm(tvecs))
 
-# Undistort an example image.
-# Make sure to use an image that exists in your directory.
-img = cv.imread(imagesFolder+imageToUndistort)
-h, w = img.shape[:2]
-newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+for t in tvecs:
+    print(f"distance is {np.linalg.norm(t)}")
 
-# Undistort the image.
-dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+# # Undistort an example image.
+# # Make sure to use an image that exists in your directory.
+# img = cv.imread(imagesFolder+imageToUndistort)
+# h, w = img.shape[:2]
+# newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
 
-# Crop the image based on the ROI.
-x, y, w, h = roi
-# disable cropping
-#dst = dst[y:y+h, x:x+w]
-cv.imwrite(calibrationResultImage, dst)
+# # Undistort the image.
+# dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+
+# # Crop the image based on the ROI.
+# x, y, w, h = roi
+# # disable cropping
+# #dst = dst[y:y+h, x:x+w]
+# cv.imwrite(calibrationResultImage, dst)
 
 # Calculate the total error of the calibration.
 total_error = 0
@@ -150,4 +108,6 @@ for i in range(len(objpoints)):
 print("Total error:", total_error / len(objpoints))
 
 # Save the camera matrix and the distortion coefficients to be used for future images.
-np.savez(calibrationData+'.npz', camera_matrix=mtx, dist_coeffs=dist, rvecs=rvecs, tvecs=tvecs)
+np.savez(os.path.join(outputPath, f"{outputName}.npz"), camera_matrix=mtx, dist_coeffs=dist, rvecs=rvecs, tvecs=tvecs)
+# Save also local copy for backup
+np.savez( os.path.join(script_dir, "results", f"{outputName}.npz") , camera_matrix=mtx, dist_coeffs=dist, rvecs=rvecs, tvecs=tvecs)
